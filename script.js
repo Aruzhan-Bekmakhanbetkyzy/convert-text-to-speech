ad8498d975bf2f9bd0722959068c5e48c50f4318
Aruzhan-Bekmakhanbetkyzy <aruzhanbekmakhanbetkyzy@gmail.com>
2022-09-08 04:25:16 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const textarea = document.querySelector("#text");
+let voiceList = document.querySelector("#voice");
+let speechBtn = document.querySelector(".submit");
+
+let synth = speechSynthesis
+let isSpeaking = true;
+
+function voiceSpeech () {
+    for (let voice of synth.getVoices()) {
+        let option = document.createElement('option')
+        option.text = voice.name 
+        voiceList.add(option)
+         console.log(option)
+    }
+}
